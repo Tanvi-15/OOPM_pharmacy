9c7bc77f881b6d2845be56df3c9facc43af3aae1
tanvi-15 <tanvideshpandedc@gmail.com>
2021-11-27 13:11:08 +0530

    Medicine and MedDatabase updated

--- PMS/Medicine.java
+++ PMS/Medicine.java
@@ -1,4 +1,25 @@
 package PMS;
+import java.util.*;
+
 public class Medicine {
-    
+    public String name;// title
+    public String company;// author //company
+    public int quantity;
+    public String expiry_date ;//ISBN
+    public double price;
+
+    //construct
+    Medicine (String name, String company, int quantity, String expiry_date, double price) {
+        this.name = name;
+        this.company = company;
+        this.quantity=quantity;
+        this.expiry_date=expiry_date ;
+        this.price=price ;
+        
+    }
+
+    public void getMedicineDetails() { //getBookDetails
+        System.out.print(name + "\t" + company + "\t" +quantity+ "\t\t" + expiry_date + "\t" + price + "\t" );
+    }
+
 }
--- PMS/MedicineDatabase.java
+++ PMS/MedicineDatabase.java
@@ -1,4 +1,37 @@
 package PMS;
+import java.util.*;
+
 public class MedicineDatabase {
+    public static Vector<Medicine> all_medicines = new Vector<Medicine>();
+
+    public static void addMedicine(Medicine b) {
+        all_medicines.add(b);
+        System.out.println("Medicine successfully added to system.");
+    }
+
+    public static void deleteMedicine(String title) {
+        for (int i = 0; i < all_medicines.size(); i++) {
+            if (title.equalsIgnoreCase(all_medicines.get(i).name)) {
+                all_medicines.remove(i);
+                System.out.println("Medicine successfully deleted from system.");
+            }
+        }
+    }
+
+    public static void displayDatabase() {
+        System.out.println("Name\tCompany\tQuantity\tExpiry\t\tPrice");
+        for (int i = 0; i < all_medicines.size(); i++) {
+            all_medicines.get(i).getMedicineDetails();
+            System.out.println();
+        }
+    }
+    public static String search(String given_name) {
+        for (int i = 0; i < all_medicines.size(); i++) {
+            if (given_name.equalsIgnoreCase(all_medicines.get(i).name)) {
+                return given_name;
+            } 
+        }
+        return "";
+    }
     
 }
