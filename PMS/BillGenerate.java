f5d24b7fdc3af36cf33535536229c2211e455870
IshikaDe-2803 <dmishika2002@gmail.com>
2021-11-27 15:07:22 +0530

    Minor changes

--- PMS/BillGenerate.java
+++ PMS/BillGenerate.java
@@ -3,3 +3,8 @@ package PMS;
 public class BillGenerate {
     
 }
+/*
+Input: customer name, date, phone number, doctor
+Additional Input: Medicine, Quantity, (price, auto generate)
+Calculate: Total Price
+*/
--- PMS/BillRecords.java
+++ PMS/BillRecords.java
@@ -1,5 +1,9 @@
 package PMS;
-
+import java.util.*;
 public class BillRecords {
-    
+    public static Vector<Medicine> all_medicines = new Vector<Medicine>();
 }
+/*
+-> display BillRecords
+-> search bills
+*/
