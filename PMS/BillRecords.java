1e7d54b46f83a5698c9c350f2a93853b2b7c0861
IshikaDe-2803 <dmishika2002@gmail.com>
2021-11-27 15:22:12 +0530

    Added billrecord details

--- PMS/BillRecords.java
+++ PMS/BillRecords.java
@@ -1,9 +1,24 @@
 package PMS;
 import java.util.*;
 public class BillRecords {
-    public static Vector<Medicine> all_medicines = new Vector<Medicine>();
+    public static Vector<BillGenerate> all_Bills = new Vector<BillGenerate>();
+    public static void addBill(BillGenerate b) {
+        all_Bills.add(b);
+        System.out.println("Bill successfully added to system.");
+    }
+    public static void displayBills() {
+        System.out.println("Name\tCompany\tQuantity\tExpiry\t\tPrice");
+        for (int i = 0; i < all_Bills.size(); i++) {
+            all_Bills.get(i).getBillDetails();
+            System.out.println();
+        }
+    }
+    public static boolean search(String given_contact) {
+        for (int i = 0; i < all_Bills.size(); i++) {
+            if (given_contact.equals(all_Bills.get(i).contact)) {
+                return true;
+            } 
+        }
+        return false;
+    }
 }
-/*
--> display BillRecords
--> search bills
-*/
